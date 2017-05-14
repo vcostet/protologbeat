84889e56d38b3344a8bfc5bc00668a662e17894d
Al Lefebvre <alain.lefebvre@gmail.com>
2017-05-14 07:01:49 -0400

    Adding support for GELF formated input messages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 
+### Version 0.2.0
+- Added support for receiving GELF format messages by setting `enable_gelf: true`
+
 ### Version 0.1.1
 - Added Dockerfile and seperate `protologbeat-docker.yml` config file to be used by docker image
 - Updated default `protologbeat.yml` to have bare-minimum config values
--- protologbeat_test.go
+++ protologbeat_test.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"testing"
+	"time"
 
-	"github.com/Graylog2/go-gelf/gelf"
+	"github.com/elastic/beats/libbeat/common"
 	"github.com/hartfordfive/protologbeat/config"
 	"github.com/hartfordfive/protologbeat/protolog"
+
+	"github.com/Graylog2/go-gelf/gelf"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGreylogReceive(t *testing.T) {
 
 	var logEntriesRecieved chan common.MapStr
-	var logEntriesError chan bool
+	var logEntriesErrors chan bool
 
-	ll := protolog.NewLogListener(config.Config{EnableGelf: true, Port: 6000})
+	logEntriesRecieved = make(chan common.MapStr, 1)
+	logEntriesErrors = make(chan bool, 1)
+
+	ll := protolog.NewLogListener(config.Config{EnableGelf: true, Port: 6000, DefaultEsLogType: "graylog"})
 
 	go func(logs chan common.MapStr, errs chan bool) {
 		ll.Start(logs, errs)
@@ -21,21 +28,54 @@ func TestGreylogReceive(t *testing.T) {
 
 	var event common.MapStr
 
-	gelf.CompressGzip
 	gw, err := gelf.NewWriter("127.0.0.1:6000")
 	if err != nil {
-		return nil, fmt.Errorf("NewWriter: %s", err)
+		t.Errorf("NewWriter: %s", err)
+		return
 	}
 	gw.CompressionType = gelf.CompressGzip
+
+	expectedVersion := "1.1"
+	expectedHost := "localhost"
+	expectedShort := "This is a test message for protologbeat"
+	expectedFull := "This is the full message expected for the test of gelf input."
+	expectedTs := float64(time.Now().Unix())
+	expectedLevel := int32(6)
+	expectedFacility := "local6"
+	exepectedType := "graylog"
+
+	if err := gw.WriteMessage(&gelf.Message{
+		Version:  expectedVersion,
+		Host:     expectedHost,
+		Short:    expectedShort,
+		Full:     expectedFull,
+		TimeUnix: expectedTs,
+		Level:    expectedLevel,
+		Facility: expectedFacility,
+		Extra:    map[string]interface{}{"type": exepectedType},
+	}); err != nil {
+		t.Errorf("Could not write message to GELF listener: %v", err)
+		return
+	}
 
 	for {
 		select {
 		case <-logEntriesErrors:
 			t.Errorf("Error receiving GELF format message")
+			return
 		case event = <-logEntriesRecieved:
-			if _, ok := event["@timestamp"]; ok {
-
+			if _, ok := event["@timestamp"]; !ok {
+				t.Errorf("Message missing timestamp field!: %v", event)
+				return
 			}
+			assert.Equal(t, event["gelf"].(map[string]interface{})["version"], expectedVersion, "Version should be the same")
+			assert.Equal(t, event["host"], expectedHost, "Host should be the same")
+			assert.Equal(t, event["short_message"], expectedShort, "Short message should be the same")
+			assert.Equal(t, event["full_message"], expectedFull, "Host should be the same")
+			assert.Equal(t, event["level"], expectedLevel, "Host should be the same")
+			assert.Equal(t, event["facility"], expectedFacility, "Host should be the same")
+			assert.Equal(t, event["type"], exepectedType, "Host should be the same")
+			return
 		}
 	}
 }
